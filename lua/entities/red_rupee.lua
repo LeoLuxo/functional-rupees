2804ef4ea41480384553e73cb1f52c33f3c81dce
LeoLuxo <leo@gamboa.eu>
2020-11-12 13:36:37 +0100

    Merge all rupee folders into single lua files

---
+++ lua/entities/red_rupee.lua
@@ -0,0 +1,62 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+ENT.Author = "LeoLuxo"
+ENT.Contact = ""
+
+ENT.PrintName = "Red Rupee (20)"
+ENT.Category = "Rupees"
+ENT.Purpose = "Gives you 20 health"
+ENT.Instructions = ""
+
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
+
+
+if SERVER then
+	
+	function ENT:Initialize()
+		self:SetModel( "models/red_rupee/red_rupee.mdl" )
+		self:PhysicsInit( SOLID_VPHYSICS )
+		self:SetMoveType( MOVETYPE_VPHYSICS )
+		self:SetSolid( SOLID_VPHYSICS )
+		self:SetUseType( SIMPLE_USE )
+		self:SetCollisionGroup( COLLISION_GROUP_WEAPON )
+		self:SetTrigger( true )
+	end
+
+	function ENT:SpawnFunction( ply, tr, ClassName )
+
+		if ( !tr.Hit ) then return end
+
+		local SpawnPos = tr.HitPos + tr.HitNormal * 16
+		local SpawnAng = ply:EyeAngles()
+		SpawnAng.p = 0
+		SpawnAng.y = SpawnAng.y + 90
+
+		local ent = ents.Create( ClassName )
+		ent:SetPos( SpawnPos )
+		ent:SetAngles( SpawnAng )
+		ent:Spawn()
+		ent:Activate()
+
+		return ent
+
+	end
+
+	function ENT:Touch( ent )
+		
+		if ( ent:IsPlayer() and ent:IsValid() ) then
+			if ( GetConVar( "gmod_functional_rupees_armor" ):GetBool() ) then
+				ent:SetArmor( ent:Armor() + 20 )
+			else
+				ent:SetHealth( ent:Health() + 20 )
+			end
+			
+			self:EmitSound( "rupee2.wav", 150, 100, 1, CHAN_AUTO )
+			self:Remove()
+		end
+		
+	end
+	
+end
